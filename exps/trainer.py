93f7f83b827e5cd387468c28964d894a8b542490
WoodFY <zfyfishwood@gmail.com>
2025-02-17 09:33:49 +0800

    Update trainer

--- exps/trainer.py
+++ exps/trainer.py
@@ -1,17 +1,19 @@
+import pandas as pd
 import torch
 
 import os
 import numpy as np
 
 from tqdm import tqdm
+from datetime import datetime
 
 from utils.metrics import compute_precision, compute_recall, compute_f1, compute_confusion_matrix, compute_specificity
 from utils.metrics_visualization import plot_metrics, plot_confusion_matrix, plot_roc_auc_curve
 
 
 def train(
-        model, exp_dir, exp_name, train_loader, valid_loader, criterion, optimizers,
-        schedulers, early_stopping, epochs, device, is_early_stopping=False, is_metrics_visualization=True
+        exp_dir, exp_model_name, model, train_loader, valid_loader, criterion, optimizers,
+        schedulers, early_stopping, epochs, device, use_early_stopping=False, metrics_visualization=True
 ):
     train_losses = []
     train_accuracies = []
@@ -25,7 +27,10 @@ def train(
         total_train_loss = 0
         total_train_correct = 0
         total_train_samples = 0
-        train_progress_bar = tqdm(train_loader, desc=f'Epoch: {epoch + 1} Training'.ljust(20), leave=True)
+
+        epoch_digits = len(str(epochs))
+        epoch_str = f'{epoch + 1}'.zfill(epoch_digits)
+        train_progress_bar = tqdm(train_loader, desc=f'Epoch: {epoch_str} Training', leave=True)
         for X, y in train_progress_bar:
             X, y = X.to(device), y.to(device)
 
@@ -52,7 +57,7 @@ def train(
         total_valid_loss = 0
         total_valid_correct = 0
         total_valid_samples = 0
-        valid_progress_bar = tqdm(valid_loader, desc=f'Epoch: {epoch + 1} Evaluating'.ljust(20), leave=True)
+        valid_progress_bar = tqdm(valid_loader, desc=f'Epoch: {epoch_str} Evaluating', leave=True)
         with torch.no_grad():
             for X, y in valid_progress_bar:
                 X, y = X.to(device), y.to(device)
@@ -79,53 +84,65 @@ def train(
         valid_losses.append(epoch_valid_loss)
         valid_accuracies.append(epoch_valid_accuracy)
         print('Epoch: {} | Train Loss: {:.4f} | Train Acc: {:.4f} | Valid Loss: {:.4f} | Valid Acc: {:.4f}'.format(
-            epoch + 1, epoch_train_loss, epoch_train_accuracy, epoch_valid_loss, epoch_valid_accuracy
+            epoch_str, epoch_train_loss, epoch_train_accuracy, epoch_valid_loss, epoch_valid_accuracy
         ))
 
         # scheduler.step(valid_loss)
         for scheduler in schedulers:
             scheduler.step(epoch_valid_loss)
 
-        if is_early_stopping:
-            early_stopping(epoch_valid_loss, model, save_model_dir=exp_dir, save_model_name=exp_name)
+        if use_early_stopping:
+            early_stopping(epoch_valid_loss, model, save_model_dir=exp_dir, save_model_name=exp_model_name)
             if early_stopping.early_stop:
                 print('Early Stopping.')
 
-                if is_metrics_visualization:
+                if metrics_visualization:
                     metrics = [
                         (train_losses, valid_losses),
                         (train_accuracies, valid_accuracies)
                     ]
-                    plot_metrics(metrics, ['Loss', 'Accuracy'], exp_name, exp_dir)
+                    plot_metrics(exp_dir, exp_model_name, metrics, ['Loss', 'Accuracy'])
 
                 break
 
         else:
-            save_model_path = os.path.join(exp_dir, exp_name)
+            save_model_path = os.path.join(exp_dir, f'{exp_model_name}.pth')
 
             if epoch_valid_loss < min_loss:
                 min_loss = epoch_valid_loss
                 torch.save(model.state_dict(), str(save_model_path))
-                print(f"Model saved at epoch {epoch}, validation loss: {min_loss:.4f}")
+                print(f"Model saved at epoch {epoch + 1}, validation loss: {min_loss:.4f}")
 
-    if is_metrics_visualization:
+    # save train and valid accuracy
+    metrics_df = pd.DataFrame({
+        'Epoch': list(range(1, len(train_accuracies) + 1)),
+        'Train Loss': train_losses,
+        'Valid Loss': valid_losses,
+        'Train Accuracy': train_accuracies,
+        'Valid Accuracy': valid_accuracies
+    })
+    time_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    metrics_df.to_csv(os.path.join(exp_dir, f'{exp_model_name}_metrics_{time_stamp}.csv'), index=False)
+    print(f'Metrics saved to {exp_dir}')
+
+    if metrics_visualization:
         metrics = [
             (train_losses, valid_losses),
             (train_accuracies, valid_accuracies),
         ]
-        plot_metrics(metrics, ['Loss', 'Accuracy'], exp_name, exp_dir)
+        plot_metrics(exp_dir, exp_model_name, metrics, ['Loss', 'Accuracy'])
 
 
 def test(
-        model, exp_dir, exp_name, test_loader, criterion, label_mapping,
-        device, metric_args=None, is_metrics_visualization=True
+        exp_dir, exp_model_name, model, test_loader, criterion, label_mapping,
+        device, metrics_visualization=True
 ):
-    save_model_path = os.path.join(exp_dir, exp_name)
+    save_model_path = os.path.join(exp_dir, f'{exp_model_name}.pth')
 
-    if save_model_path is None:
+    if not os.path.exists(save_model_path):
         raise FileNotFoundError('No models checkpoint file found in the directory.')
 
-    model.load_state_dict(torch.load(str(save_model_path)))
+    model.load_state_dict(torch.load(str(save_model_path), weights_only=True))
 
     model.eval()
     total_test_loss = 0
@@ -163,17 +180,17 @@ def test(
     print('==========================================================================================')
     print('Test Loss: {:.4f} | Test Acc: {:.4f}'.format(test_loss, test_accuracy))
     print('Test Precision: {:.4f} | Test Recall (Sensitivity): :{:.4f} | Test F1: {:.4f}'.format(
-        test_precision, test_recall, test_f1))
+        test_precision, test_recall, test_f1)
+    )
 
     confusion_matrix, _ = compute_confusion_matrix(all_labels, all_predicts, label_mapping)
     # print(f'Confusion Matrix: \n{matrix}')
-
     specificity = compute_specificity(confusion_matrix, label_mapping)
     print(f'Specificity for each class: {specificity}')
     print('==========================================================================================')
 
-    if is_metrics_visualization:
-        plot_confusion_matrix(all_labels, all_predicts, label_mapping, exp_name, exp_dir, cm=confusion_matrix)
-        plot_roc_auc_curve(np.array(all_labels), np.array(all_predicts_proba), label_mapping, exp_name, exp_dir)
+    if metrics_visualization:
+        plot_confusion_matrix(all_labels, all_predicts, label_mapping, exp_model_name, exp_dir, cm=confusion_matrix)
+        plot_roc_auc_curve(np.array(all_labels), np.array(all_predicts_proba), label_mapping, exp_model_name, exp_dir)
 
     return test_accuracy, test_precision, test_recall, test_f1
